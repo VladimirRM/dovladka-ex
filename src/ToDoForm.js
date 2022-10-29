2abbf815933bf6d12c8cdece7b0905cde4093376
Vladimir RM <shvdrummer@gmail.com>
2022-10-29 13:51:25 +0300

    add onChange={handleChange}/>

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,10 +2,14 @@ import React,{useState} from 'react'
 
 const ToDoForm = () => {
     const [userInput, setUserInput]=useState('')
+    const handleSubmit =()=>{
+
+    }
   return (
     <div>
         <form onSubmit={handleSubmit}>
-
+             <input value={userInput} type='text' 
+             onChange={handleChange}/>
         </form>
     </div>
   )
