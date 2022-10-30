408367acf7e80f8fa6538552d27b2dd9b87bc50c
Vladimir RM <shvdrummer@gmail.com>
2022-10-30 08:00:48 +0200

    add [rops addtask

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-
 import React, { useState } from "react";
-import ToDo from './ToDo'
-import ToDoForm  from "./ToDoForm";
+import ToDo from "./ToDo";
+import ToDoForm from "./ToDoForm";
 
 function App() {
-   const [ todos,setTodos]=useState([])
-   const addTasks =(userInput)=>{
-      if(userInput){
-        const newItem={
-          id:Math.random().toString(36).substr(2.9),
-          task: userInput,
-          complete: false 
-        }
-        setTodos([...todos, newItem])
-      }
-   }
-   const removeTask =()=>{
-
-   }
-   const handleToggle =()=>{
-
-   }
+  const [todos, setTodos] = useState([]);
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2,9),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+  const removeTask = () => {};
+  const handleToggle = () => {};
   return (
-<div className="App">
-  <header>
-    <h1>Todo list:{todos.length}</h1>
-    </header>
-    <ToDoForm/>
-    {todos .map((todo)=>{
-      return(
-        <ToDo key={todo.id}
-        todo={todo}
-        toggleTask={handleToggle}
-        removeTask={removeTask}
-        />
-       
-      )
-    })}
-</div>
+    <div className="App">
+      <header>
+        <h1>Todo list:{todos.length}</h1>
+      </header>
+      <ToDoForm  addTask={addTask}/>
+      {todos.map((todo) => {
+        return (
+          <ToDo
+            key={todo.id}
+            todo={todo}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+          />
+        );
+      })}
+    </div>
   );
 }
 
