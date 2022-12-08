fe0c53cb7503fa8d48363e9d3a1adf916da8f950
Vladimir RM <shvdrummer@gmail.com>
2022-12-08 09:23:39 +0200

    create input

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,13 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const ToDo = () => {
-  const [userInput,setUserInput]=useState('')
+  const [userInput, setUserInput] = useState("");
   return (
     <form onSubmit={handleSubmit}>
-         
-
+      <input
+        type="text"
+        onChange={handleChange}
+        value={userInput}
+        onKeyDown={handleKeyPress}
+        placeholder="Enter something"
+      />
     </form>
-  )
-}
+  );
+};
 
-export default ToDo
+export default ToDo;
