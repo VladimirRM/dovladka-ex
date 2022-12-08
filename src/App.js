e034f0f6798020144f105c02f321ae38513e1fe8
Vladimir RM <shvdrummer@gmail.com>
2022-12-08 09:16:00 +0200

    add main inport in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React,{useState} from 'react'
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
+import './index.css'
 
 const App = () => {
   const [todos,setTodos]=useState([])
@@ -17,7 +20,8 @@ const App = () => {
       <header>
         <h1>Todo List:{todos.length}</h1>
       </header>
-      
+      T
+
       
     </div>
   )
