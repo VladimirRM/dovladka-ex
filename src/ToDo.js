de8cde12c3ff385cecae7cfe567e299d6590893b
Vladimir RM <shvdrummer@gmail.com>
2022-12-08 09:25:06 +0200

    add main func in todo form

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 
 const ToDo = () => {
+
+const handleChange =()=>{
+  
+}
+const handleSubmit =()=>{
+
+}
+const handleKeyPress =()=>{
+
+}
+
+
+
   const [userInput, setUserInput] = useState("");
   return (
     <form onSubmit={handleSubmit}>
